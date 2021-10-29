339f47167150138cd352fe6e5131ad32dfa6f576
nkostenko <nikita.kostenk@emergn.com>
2021-10-29 16:42:05 +0300

    The new list view component has been reused. Added handling of cases of missing data and loading. Columns added. Reused github api service

--- src/app/features/git-repositories/git-repositories.component.ts
+++ src/app/features/git-repositories/git-repositories.component.ts
@@ -1,4 +1,8 @@
 import { Component, OnInit } from '@angular/core';
+import { GitPublicApiService } from '@libs/services';
+import { catchError, finalize } from 'rxjs/operators';
+import { of } from 'rxjs';
+import { ColumnDto } from '@libs/common';
 
 @Component({
 	selector: 'git-repositories',
@@ -7,9 +11,52 @@ import { Component, OnInit } from '@angular/core';
 })
 export class GitRepositoriesComponent implements OnInit {
 
-	constructor() { }
+	public repositories: Array<any> = [];
+	public totalCount: number = 0;
+	public isLoading = true;
+	public hasData = false;
 
-	ngOnInit(): void {
+	public columns: ColumnDto[] = [
+		{
+			header: 'Repository name',
+			ngModelName: 'name',
+			sort: false
+		},
+		{
+			header: 'Score',
+			ngModelName: 'score',
+			sort: false
+		}
+	]
+
+	constructor(private gitPublicApiService: GitPublicApiService) {
 	}
 
+	ngOnInit(): void {
+		this.loadRepositories();
+	}
+
+	private loadRepositories(): void {
+		this.isLoading = true;
+		this.gitPublicApiService.getRepositories()
+			.pipe(
+				finalize(() => {
+					this.isLoading = false;
+					this.hasData = !!this.repositories && this.repositories.length > 0;
+				}),
+				catchError((error, caught) => {
+					console.warn(error);
+					// TODO ADD EROR HANDLER WITH MODAL
+					return of(null);
+				})
+			).subscribe((data) => {
+				if (!!data) {
+					this.repositories = data.items;
+					this.totalCount = data.total_count;
+				} else {
+					this.repositories = [];
+					this.totalCount = 0;
+				}
+			});
+	}
 }
