b54fcb321595b02ffc54bd01a5eb376e1d9c1e50
nkostenko <nikita.kostenk@emergn.com>
2021-10-31 21:46:10 +0300

    Reused filter config. Added filter logic

--- src/libs/common/src/lib/common.module.ts
+++ src/libs/common/src/lib/common.module.ts
@@ -3,11 +3,15 @@ import { CommonModule } from '@angular/common';
 import { FilterWrapperComponent } from './filter-wrapper/filter-wrapper.component';
 import { ListViewComponent } from './list-view/list-view.component';
 import { TableModule } from 'primeng/table';
+import { InputTextModule } from 'primeng/inputtext';
+import { FormsModule } from '@angular/forms';
 
 @NgModule({
 	imports: [
 		CommonModule,
 		TableModule,
+		InputTextModule,
+		FormsModule,
 	],
 	exports: [FilterWrapperComponent, ListViewComponent],
 	declarations: [FilterWrapperComponent, ListViewComponent]
--- src/libs/common/src/lib/filter-wrapper/filter-wrapper.component.ts
+++ src/libs/common/src/lib/filter-wrapper/filter-wrapper.component.ts
@@ -1,15 +1,26 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, EventEmitter, Input, Output } from '@angular/core';
+import { IFilterConfig } from '@libs/common';
 
 @Component({
 	selector: 'filter-wrapper',
 	templateUrl: './filter-wrapper.component.html',
 	styleUrls: ['./filter-wrapper.component.css']
 })
-export class FilterWrapperComponent implements OnInit {
+export class FilterWrapperComponent<T> {
 
-	constructor() { }
+	@Input()
+	public filterConfig!: IFilterConfig<T>;
 
-	ngOnInit(): void {
+	@Output()
+	public onFilterChanged: EventEmitter<T> = new EventEmitter<T>();
+
+	constructor() {
+	}
+
+	public onApplyClicked(): void {
+		if (!!this.filterConfig.applyFilterValidator && this.filterConfig.applyFilterValidator(this.filterConfig)) {
+			this.onFilterChanged.emit(this.filterConfig.resultDto);
+		}
 	}
 
 }
