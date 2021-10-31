7502fef9b8064f16ed541dc6f3f0dbe94662b2fc
nkostenko <nikita.kostenk@emergn.com>
2021-11-01 00:53:07 +0300

    Added export in public for filter and table

--- src/libs/common/src/public_api.ts
+++ src/libs/common/src/public_api.ts
@@ -1,7 +1,9 @@
 export * from './lib/common.module';
 
+export * from './lib/st-table/st-table.component';
 export * from './lib/st-table/dto/IColumnConfig';
 
+export * from './lib/st-filter/st-filter.component';
 export * from './lib/st-filter/dto/filter-config';
 
 export * from './lib/st-configurable-modal/st-configurable-modal.component';
