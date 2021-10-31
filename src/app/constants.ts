5859f8b6efe5030493f0be13d9ff97ba6ed1235b
nkostenko <nikita.kostenk@emergn.com>
2021-10-31 16:31:00 +0300

    added constants

---
+++ src/app/constants.ts
@@ -0,0 +1,8 @@
+const DEFAULT_GIT_API = 'https://api.github.com';
+const DEFAULT_GIT_SEARCH_API = `${DEFAULT_GIT_API}/search`;
+
+export const URL_CONSTANTS = {
+	GIT_SEARCH: {
+		REPOSITORIES: `${DEFAULT_GIT_SEARCH_API}/repositories`,
+	}
+}
