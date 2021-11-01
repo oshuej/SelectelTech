faaf3e9b17d4aa299843df0210e9c0579fe87a71
nkostenko <nikita.kostenk@emergn.com>
2021-11-01 04:29:26 +0300

    IFilterFieldsConfig renamed to IFilterFieldConfig

--- src/libs/common/src/lib/st-filter/dto/filter-config.ts
+++ src/libs/common/src/lib/st-filter/dto/filter-config.ts
@@ -1,11 +1,11 @@
 
 export interface IFilterConfig<T> {
-	fields: Array<IFilterFieldsConfig<T>>;
+	fields: Array<IFilterFieldConfig<T>>;
 	resultDto: T;
 	applyFilterValidator?: (filterConfig: IFilterConfig<T>) => boolean;
 }
 
-export interface IFilterFieldsConfig<T> {
+export interface IFilterFieldConfig<T> {
 	label: string;
 	ngModelName: keyof T;
 	required?: boolean;
